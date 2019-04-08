55727441740f51376ea52113c99dfa8eeb7cbf4a
aaronandyhealy <aaronhealy2010@hotmail.com>
2019-04-08 18:09:26 +0100

    Pie Chart Working,need to import data for it

--- Frontend/final-year/src/app/home/home.component.ts
+++ Frontend/final-year/src/app/home/home.component.ts
@@ -4,6 +4,14 @@ import { TokenStorageService } from '../auth/token-storage.service';
 import { Order } from '../_models/order';
 import { OrderService } from '../_services/order.service';
 import { CustomerService } from '../_services/customer.service';
+
+import { Component, NgZone } from "@angular/core";
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+am4core.useTheme(am4themes_animated);
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,8 +20,9 @@ import { CustomerService } from '../_services/customer.service';
 export class HomeComponent implements OnInit {
   info: any;
   orders: Observable<Order[]>;
+  private chart: am4charts.PieChart;
 
-  constructor(private token: TokenStorageService, private customerService: CustomerService, private orderService: OrderService) { }
+  constructor(private token: TokenStorageService,private zone: NgZone, private customerService: CustomerService, private orderService: OrderService) { }
 
   ngOnInit() {
     this.info = {
@@ -21,8 +30,39 @@ export class HomeComponent implements OnInit {
       username: this.token.getUsername(),
       authorities: this.token.getAuthorities()
     };
+
     this.reloadData();
   }
+  ngAfterViewInit() {
+    this.zone.runOutsideAngular(() => {
+      let chart = am4core.create("chartdiv", am4charts.PieChart);
+
+      chart.paddingRight = 20;
+
+      let data = [];
+     
+// Add data
+chart.data = [{
+  "country": "Lithuania",
+  "litres": 501.9
+}];
+
+// Add and configure Series
+let pieSeries = chart.series.push(new am4charts.PieSeries());
+pieSeries.dataFields.value = "litres";
+pieSeries.dataFields.category = "country";
+      this.chart = chart;
+    });
+  }
+
+  ngOnDestroy() {
+    this.zone.runOutsideAngular(() => {
+      if (this.chart) {
+        this.chart.dispose();
+      }
+    });
+  }
+
 
   logout() {
     this.token.signOut();
